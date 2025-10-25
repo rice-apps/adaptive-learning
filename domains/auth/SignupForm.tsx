9a16bcd58a0a5832ed5e640bec40c4143b8df95a
moyindavid16 <moyindavid16@gmail.com>
2025-10-25 15:49:20 -0500

    sign up logic

--- domains/auth/SignupForm.tsx
+++ domains/auth/SignupForm.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { useState } from "react";
-import { UserRole } from "./types";
+import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
+import {useState} from "react";
+import {UserRole} from "./types";
+import {createClient} from "@/lib/supabase/client";
+import {toast} from "sonner";
 
 export default function SignupForm() {
   const [email, setEmail] = useState("");
@@ -12,14 +14,66 @@ export default function SignupForm() {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [role, setRole] = useState("student");
 
+  const verifyFields = () => {
+    if (!email) {
+      toast.error("Email is required");
+      return false;
+    }
+    if (!password) {
+      toast.error("Password is required");
+      return false;
+    }
+    if (!confirmPassword) {
+      toast.error("Confirm Password is required");
+      return false;
+    }
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match");
+      return false;
+    }
+    if (!role) {
+      toast.error("Role is required");
+      return false;
+    }
+    return true;
+  };
+
+  const handleSignup = async () => {
+    if (!verifyFields()) {
+      return;
+    }
+    const supabase = await createClient();
+    const {error} = await supabase.auth.signUp({
+      email: email,
+      password: password,
+      options: {
+        data: {
+          role: role,
+        },
+      },
+    });
+
+    if (error) {
+      console.error(error);
+      toast.error("Something went wrong");
+      return;
+    }
+
+    toast.success("Signup successful");
+    console.log("Signup worked");
+  };
+
   return (
     <div className="flex flex-col gap-5 w-1/3 h-1/2 bg-white rounded-lg border p-8">
+      <h1>Sign Up</h1>
       <FormItem placeholder="Email" input={email} setInput={setEmail} />
-      <FormItem placeholder="Password" input={password} setInput={setPassword} />
-      <FormItem placeholder="Confirm Password" input={confirmPassword} setInput={setConfirmPassword} />
+      <FormPassword placeholder="Password" input={password} setInput={setPassword} />
+      <FormPassword placeholder="Confirm Password" input={confirmPassword} setInput={setConfirmPassword} />
       <RoleSelect role={role} setRole={setRole} />
       <div className="flex flex-wrap items-center gap-4 md:flex-row">
-        <Button variant="outline">Sign Up</Button>
+        <Button variant="outline" onClick={handleSignup}>
+          Sign Up
+        </Button>
       </div>
     </div>
   );
@@ -46,6 +100,27 @@ const FormItem = ({
   );
 };
 
+const FormPassword = ({
+  placeholder,
+  input,
+  setInput,
+}: {
+  placeholder: string;
+  input: string;
+  setInput: (input: string) => void;
+}) => {
+  return (
+    <Input
+      type="password"
+      id="textBox"
+      placeholder={placeholder}
+      value={input}
+      onChange={e => setInput(e.target.value)}
+      required
+    />
+  );
+};
+
 const RoleSelect = ({role, setRole}: {role: string; setRole: (role: string) => void}) => {
   const roles = Object.values(UserRole);
   return (
@@ -54,8 +129,10 @@ const RoleSelect = ({role, setRole}: {role: string; setRole: (role: string) => v
         <SelectValue placeholder="Select a role" />
       </SelectTrigger>
       <SelectContent>
-        {roles.map((role) => (
-          <SelectItem key={role} value={role}>{role}</SelectItem>
+        {roles.map(role => (
+          <SelectItem key={role} value={role}>
+            {role}
+          </SelectItem>
         ))}
       </SelectContent>
     </Select>
