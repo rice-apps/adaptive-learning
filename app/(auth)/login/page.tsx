e4c36e31d3ca68fa884b37c6dec5993b9d9e6004
ashleyniu <an110@rice.edu>
2025-11-01 17:51:58 -0500

    login form and logic

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import SignupForm from "@/domains/auth/SignupForm";
+
+export default function Signup() {
+  return (
+    <div className="h-screen w-full flex bg-slate-100 items-center justify-center">
+      <SignupForm />
+    </div>
+  );
+}
