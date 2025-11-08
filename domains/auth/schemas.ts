915af8ef2ee90cd105f4c710f4f1fac86b776507
AnamElav <thinkerdoodl@gmail.com>
2025-11-08 14:37:35 -0600

    use react-hook-form, zod for validation schema, shadcn form component

---
+++ domains/auth/schemas.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export const signupSchema = z
+  .object({
+    email: z.string().email({ message: "Invalid email" }),
+    password: z
+      .string()
+      .min(6, { message: "Password must be at least 6 characters" }),
+    confirmPassword: z.string(),
+    role: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
+export type SignupFormData = z.infer<typeof signupSchema>;
