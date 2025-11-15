cbf00b1b8d3179d2be7faecc51b8f7982ef78aa9
moyindavid16 <moyindavid16@gmail.com>
2025-11-15 13:49:45 -0600

    first pass quiz creation

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -50,7 +50,7 @@ export async function updateSession(request: NextRequest) {
     // no user, potentially respond by redirecting the user to the signup page
     const url = request.nextUrl.clone();
     url.pathname = "/signup";
-    return NextResponse.redirect(url);
+    // return NextResponse.redirect(url);
   }
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is.
