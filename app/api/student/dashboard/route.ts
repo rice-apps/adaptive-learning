696665e99dc700932274c76673ad94263c7b5bd8
Esenia Hernandez <eseniahernandez@Esenias-MacBook-Pro.local>
2025-11-22 15:18:50 -0600

    Finished route

--- app/api/student/dashboard/route.ts
+++ app/api/student/dashboard/route.ts
@@ -2,19 +2,78 @@ import { createClient } from '@supabase/supabase-js'
 import { NextResponse } from 'next/server'
 
 const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKEY = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
+const supabaseKEY = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
 
 if (!supabaseURL || !supabaseKEY) {
-    throw new Error('missing supabase enviornment variables')
+    throw new Error('missing Supabase environment variables');
 }
 
-const supbase = createClient (
+const supabase = createClient (
     supabaseURL,
     supabaseKEY
 );
 
-export async function GET (request: Request) {
+export async function GET(request: Request) {
     try {
-        const { data: students, error } = await supbase
+        const { searchParams } = new URL(request.url);
+        const studentId = searchParams.get('studentId');
+
+        if (!studentId) {
+            return NextResponse.json(
+                { error: 'Student ID is required' },
+                { status: 400 }
+            );
+        }
+        
+        const { data: student, error } = await supabase
+        .from('Students')
+        .select(`
+            id,
+            email,
+            progress,
+            status:isActive,
+            name:profileName,
+            avatar,
+            current_level,
+            grade_reading,
+            grade_math,
+            learning_style,
+            plan,
+            timeSpent,
+            isActive,
+            lastActive,
+            created_at 
+        `)
+        .eq('id', studentId)
+        .single();
+
+        // 056e6ece-2416-4be1-88aa-aa1d26faf8b1
+
+        // Checks for database errors 
+        if (error) {
+            console.error(error);
+            return NextResponse.json(
+                { error: "Failed to fetch students" },
+                { status: 500}
+            );
+        }
+
+        // Checks if student is not found 
+        if (!student) {
+            return NextResponse.json(
+                { error: 'Student not found' },
+                { status: 404 }
+            );
+        }
+
+        return NextResponse.json({ student });
+    }
+    
+    catch (error) {
+    console.error('Error fetching student data:', error);
+    return NextResponse.json(
+        { error: 'Internal server error' },
+        { status: 500 }
+        );
     }
 }
